643e2a0618ae184ea667fd157e96053ff1c2663c
Kirk <kirk.w.wang@gmail.com>
2018-10-16 17:25:17 +0800

    example
    update

--- src/sagas.js
+++ src/sagas.js
@@ -1,5 +1,14 @@
 import { delay } from "redux-saga";
-import { call, put, takeEvery, all, select, take } from "redux-saga/effects";
+import {
+  call,
+  put,
+  takeEvery,
+  all,
+  select,
+  take,
+  race,
+  cancelled
+} from "redux-saga/effects";
 
 export function* helloSaga() {
   // const num = yield 1;
@@ -22,22 +31,49 @@ export function* watchIncrementAsync() {
   yield takeEvery("INCREMENT_ASYNC", incrementAsync);
 }
 
-// function* watchAndLog() {
-//   yield takeEvery("*", function* logger(action) {
-//     const state = yield select();
-
-//     console.log("action", action);
-//     console.log("state after", state);
-//   });
-// }
-
 function* watchAndLog() {
-  while (true) {
-    const action = yield take("*");
+  yield takeEvery("*", function* logger(action) {
     const state = yield select();
 
-    console.log("action", action);
-    console.log("state after", state);
+    // console.log("action", action);
+    // console.log("state after", state);
+  });
+}
+
+function* play() {
+  try {
+    while (true) {
+      const action = yield take("INCREMENT");
+      const state = yield select();
+      if (state == 5) {
+        break;
+      }
+    }
+    return true;
+  } catch (err) {
+    throw err;
+  } finally {
+    if (yield cancelled()) {
+      console.log("You Lost!");
+    }
+  }
+}
+
+// 必须在 3 秒内加到 5
+function* game() {
+  while (true) {
+    console.log("Start");
+    console.log("3s……");
+    const { timeout, score } = yield race({
+      timeout: call(delay, 3000),
+      score: call(play)
+    });
+    if (score) {
+      console.log("You Win!");
+    }
+    yield put({ type: "RESET" });
+    console.log("after 2 s，Restart");
+    yield call(delay, 2000);
   }
 }
 
